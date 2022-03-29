cbba9af71678b74669bfba50925c6475aa417a0b
Sean Shubin <sshubin@cj.com>
2022-03-29 12:14:19 -0700

    support for relative dates

--- src/library/date-time-util.js
+++ src/library/date-time-util.js
@@ -1,4 +1,4 @@
-const createParser = text => {
+const createTokenizer = text => {
     let index = 0
     const isDigit = ch => {
         if (ch < '0') return false
@@ -46,15 +46,78 @@ const minMax = (min, max, actual) => {
     return Math.max(min, Math.min(max, actual))
 }
 
-const createValidator = text => {
-    const parser = createParser(text)
-    const year = minMax(1900, 9999, add2000ifLessThan1000(parser.digits(4)))
-    const month = minMax(1, 12, parser.digits(2))
-    const day = minMax(1, 31, parser.digits(2))
-    const hour = minMax(0, 23, parser.digits(2))
-    const minute = minMax(0, 59, parser.digits(2))
-    const second = minMax(0, 59, parser.digits(2))
+const relativeDateMatchRegex = /^\d+\s*[A-Za-z]+(\s*\d+\s*[A-Za-z]+)*$/
+const relativeDatePartRegex = /(\d+)\s*([A-Za-z]+)/g
+
+const isRelativeDate = text => {
+    if(text.toLowerCase() === "now") return true
+    if(text.toLowerCase() === "today") return true
+    if(text.toLowerCase() === "tomorrow") return true
+    return relativeDateMatchRegex.test(text)
+}
+
+const modifyDateFunction = {
+    second:[Date.prototype.getSeconds, Date.prototype.setSeconds],
+    minute:[Date.prototype.getMinutes, Date.prototype.setMinutes],
+    hour:[Date.prototype.getHours, Date.prototype.setHours],
+    day:[Date.prototype.getDate, Date.prototype.setDate],
+    month:[Date.prototype.getMonth, Date.prototype.setMonth],
+    year:[Date.prototype.getFullYear, Date.prototype.setFullYear],
+    seconds:[Date.prototype.getSeconds, Date.prototype.setSeconds],
+    minutes:[Date.prototype.getMinutes, Date.prototype.setMinutes],
+    hours:[Date.prototype.getHours, Date.prototype.setHours],
+    days:[Date.prototype.getDate, Date.prototype.setDate],
+    months:[Date.prototype.getMonth, Date.prototype.setMonth],
+    years:[Date.prototype.getFullYear, Date.prototype.setFullYear]
+}
+
+const parseRelativeDate = ({text, now}) => {
+    const trimmed = text.trim()
+    if(trimmed.toLowerCase() === "now") return now
+    if(trimmed.toLowerCase() === "today") return now
+    if(trimmed.toLowerCase() === "tomorrow") {
+        const newDate = new Date(now.getTime())
+        newDate.setDate(now.getDate()+1)
+        return newDate
+    }
+    const newDate = new Date(now.getTime())
+    const matches = trimmed.matchAll(relativeDatePartRegex)
+    for(const match of matches){
+        const modifyAmount = parseInt(match[1])
+        const modifyFunctionName = match[2]
+        if(modifyDateFunction.hasOwnProperty(modifyFunctionName)){
+            const [getAmount, setAmount] = modifyDateFunction[modifyFunctionName]
+            const oldAmount = getAmount.call(newDate)
+            const newAmount = oldAmount + modifyAmount
+            setAmount.call(newDate, newAmount)
+        }
+    }
+    return newDate
+}
+
+const parseDateFromTokens = text => {
+    const tokenizer = createTokenizer(text)
+    const year = minMax(1900, 9999, add2000ifLessThan1000(tokenizer.digits(4)))
+    const month = minMax(1, 12, tokenizer.digits(2))
+    const day = minMax(1, 31, tokenizer.digits(2))
+    const hour = minMax(0, 23, tokenizer.digits(2))
+    const minute = minMax(0, 59, tokenizer.digits(2))
+    const second = minMax(0, 59, tokenizer.digits(2))
     const date = new Date(year, month - 1, day, hour, minute, second)
+    return date
+}
+
+const parseDate = rawText => {
+    const text = rawText.trim()
+    if(isRelativeDate(text)){
+        return parseRelativeDate({text, now:new Date()})
+    } else {
+        return parseDateFromTokens(text)
+    }
+}
+
+const createValidator = text => {
+    const date = parseDate(text)
     return dateToParts(date)
 }
 
@@ -77,13 +140,13 @@ const dateToParts = date => {
 
 const userDateToWellFormed = value => {
     if(value == null) return null
-    if(value.search(/\d/) === -1) return null
+    if(value.trim() === '') return null
     return createValidator(value).wellFormed
 }
 
 const userDateToIso= value => {
     if(value == null) return null
-    if(value.search(/\d/) === -1) return null
+    if(value.trim() === '') return null
     return createValidator(value).isoDate
 }
 
@@ -99,13 +162,13 @@ const isoDateToLocal = value => {
 
 const userDateToUtc = value => {
     if(value == null) return null
-    if(value.search(/\d/) === -1) return null
+    if(value.trim() === '') return null
     return createValidator(value).utcDate
 }
 
 const userDateToLocal = value => {
     if(value == null) return null
-    if(value.search(/\d/) === -1) return null
+    if(value.trim() === '') return null
     return createValidator(value).localDate
 }
 
